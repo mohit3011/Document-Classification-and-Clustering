f06992d95e0711e9c7c0bcc9f6f0f328c8021e74
Alakh Desai <desaizohan@gmail.com>
2017-10-27 04:13:57 +0530

    weighted term frequencies matrix update

--- create_vector.py
+++ create_vector.py
@@ -1,12 +1,28 @@
 import sys, os
 import numpy as np
+import copy
+import math
 from numpy import *
 from sklearn.decomposition import PCA
+
+Del=0.0000000001
+
+def Transform(data,T=0):
+	if(T==0):
+		return data  #Identity
+	if(T==1):
+		return np.log(data+Del)  #Log
+	if(T==2):
+		return math.sqrt(data)  #Square-Root
 
 file_bbc_mtx = open("bbc/bbc.mtx", "r")
 
 count_row = 0
-train_data = np.zeros((2225, 9635))	# Sparse data matrix
+
+NoWords=9635
+NoData=2225
+
+train_data = np.zeros((NoData, NoWords))	# Sparse data matrix
 
 for row in file_bbc_mtx:
 	if count_row > 1:
@@ -16,6 +32,27 @@ for row in file_bbc_mtx:
 
 	count_row += 1
 
+P = np.zeros((NoData, NoWords))
+
+W = np.zeros((NoWords))
+
+new_train_data = np.zeros((NoData, NoWords))	# Final data matrix
+
+for j in range(NoWords):
+	P[:,j]=train_data[:,j]/np.sum(train_data[:,j])
+
+for j in range(NoWords):
+	tmp=P[:,j]*np.log(P[:,j]+Del)
+	W[j]=1+np.sum(tmp)/np.log(NoData)
+
+for i in range(NoData):
+	WordSum=0
+	for j in range(NoWords):
+		new_train_data[i,j]=W[j]*Transform(train_data[i,j])
+		WordSum+=new_train_data[i,j]*new_train_data[i,j]
+	WordSum=math.sqrt(WordSum)
+	new_train_data[i,:]=new_train_data[i,:]/WordSum
+		
 words = []	# List of words
 
 file_bbc_terms = open("bbc/bbc.terms", "r")
@@ -37,12 +74,13 @@ for row in file_bbc_classes:
 
 	count_row += 1
 
-pca = PCA(n_components = 2225, svd_solver='full')
+pca = PCA(n_components = NoData, svd_solver='full')
 
-#print train_data[0]
-new_train_data = pca.fit_transform(train_data)	# new_train_data is the new dataset with PCA applied
+Final_train_data = pca.fit_transform(new_train_data)	# Final_train_data is the new dataset with PCA applied
 
-#print new_train_data[0]
+print train_data[0]
+print new_train_data[0]
+print Final_train_data[0]
 component_array = pca.components_	# Component array 2225 X 9635 , new axes are the linear combination of old axes
 
 
