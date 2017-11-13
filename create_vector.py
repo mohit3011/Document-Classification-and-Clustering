7eee190a60d90e2d0337126404a1cf4591f37e78
mohit3011 <mohit.chandra@research.iiit.ac.in>
2017-11-14 00:02:23 +0530

    Refactor the code

--- create_vector.py
+++ create_vector.py
@@ -8,7 +8,7 @@ from sklearn.mixture import GaussianMixture
 
 Del=0.0000000001
 
-def Transform(data,T=0):
+def Transform(data, T=0):
 	if(T==0):
 		return data  #Identity
 	if(T==1):
@@ -16,90 +16,125 @@ def Transform(data,T=0):
 	if(T==2):
 		return math.sqrt(data)  #Square-Root
 
-file_bbc_mtx = open("bbc/bbc.mtx", "r")
+def create_word_list():
+	file_bbc_terms = open("bbc/bbc.terms", "r")
 
-count_row = 0
+	words = []
+	for row in file_bbc_terms:
+		row = row.split("\n")
+		words.append(row[0])	# List of words
 
-NoWords=9635
-NoData=2225
+	return words
 
-train_data = np.zeros((NoData, NoWords))	# Sparse data matrix
+def perform_pca(new_train_data, no_samples):
 
-for row in file_bbc_mtx:
-	if count_row > 1:
-		row  = row.split()
-		#print row
-		train_data[int(row[1])-1][int(row[0])-1] = float(row[2])
+	pca = PCA(n_components = no_samples, svd_solver='full')
 
-	count_row += 1
+	final_train_data = pca.fit_transform(new_train_data)	# final_train_data is the new dataset with PCA applied
 
-P = np.zeros((NoData, NoWords))
+	component_array = pca.components_	# Component array 2225 X 9635 , new axes are the linear combination of old axes
 
-W = np.zeros((NoWords))
+	return final_train_data, component_array 
 
-new_train_data = np.zeros((NoData, NoWords))	# Final data matrix
 
-for j in range(NoWords):
-	P[:,j]=train_data[:,j]/np.sum(train_data[:,j])
+def gmdc(final_train_data, classes):
 
-for j in range(NoWords):
-	tmp=P[:,j]*np.log(P[:,j]+Del)
-	W[j]=1+np.sum(tmp)/np.log(NoData)
+	GMM = GaussianMixture(n_components = no_samples, covariance_type='diag')
+	GMM.fit(final_train_data, classes)
 
-for i in range(NoData):
-	WordSum=0
-	for j in range(NoWords):
-		new_train_data[i,j]=W[j]*Transform(train_data[i,j])
-		WordSum+=new_train_data[i,j]*new_train_data[i,j]
-	WordSum=math.sqrt(WordSum)
-	new_train_data[i,:]=new_train_data[i,:]/WordSum
+	
+
+
+def create_classes():
+
+	count_row = 0
+	classes = []	# List having the classes for each of the document instance
+
+	file_bbc_classes = open("bbc/bbc.classes", "r")
+
+	for row in file_bbc_classes:
+		if count_row > 3:
+			row = row.split()
+			classes.append(int(row[1]))
+
+		count_row += 1
+
+	return classes
+
+def create_weight_matrix(no_data, no_samples, train_data):
+
+	prob_matrix = np.zeros((no_samples, no_data))
+
+	weight_matrix = np.zeros((no_data))
+
+	for j in range(no_data):
+		prob_matrix[:,j] = np.copy(train_data[:,j]/np.sum(train_data[:,j]))
+
+	for j in range(no_data):
+
+		tmp = np.copy(prob_matrix[:,j]*np.log(prob_matrix[:,j]+Del))
+		weight_matrix[j] = 1+np.sum(tmp)/np.log(no_samples)
+
+	return weight_matrix
+
+def create_normalized_data(no_data, no_samples, train_data):
+
+	new_train_data = np.zeros((no_samples, no_data))	# Final data matrix
+
+	weight_matrix = create_weight_matrix(no_data, no_samples, train_data)
+
+	for i in range(no_samples):
 		
-words = []	# List of words
+		WordSum=0
+		for j in range(no_data):
 
-file_bbc_terms = open("bbc/bbc.terms", "r")
+			new_train_data[i,j] = weight_matrix[j]*Transform(train_data[i,j])
 
-for row in file_bbc_terms:
-	row = row.split("\n")
-	words.append(row[0])	# List of words
+			WordSum+=new_train_data[i,j]*new_train_data[i,j]
+		
+		WordSum=math.sqrt(WordSum)
+		
+		new_train_data[i,:]=new_train_data[i,:]/WordSum
 
-count_row = 0
-
-classes = []	# List having the classes for each of the document instance
-
-file_bbc_classes = open("bbc/bbc.classes", "r")
-
-for row in file_bbc_classes:
-	if count_row > 3:
-		row = row.split()
-		classes.append(int(row[1]))
-
-	count_row += 1
-
-pca = PCA(n_components = NoData, svd_solver='full')
-
-Final_train_data = pca.fit_transform(new_train_data)	# Final_train_data is the new dataset with PCA applied
-
-print train_data[0]
-print new_train_data[0]
-print Final_train_data[0]
-component_array = pca.components_	# Component array 2225 X 9635 , new axes are the linear combination of old axes
-GMM = GaussianMixture(n_components = NoData, covariance_type='diag')
-GMM.fit(component_array,classes)
+	return new_train_data
 
 
-'''pca_lambda = pca.explained_variance_
+def create_vectors():
+	file_bbc_mtx = open("bbc/bbc.mtx", "r")
 
-total = 0
-for k in range(len(pca_lambda)):
-	total += pca_lambda[k]
-										
-									# Code for finding out the ideal number of components to keep
-sum_k = 0
-print len(pca_lambda)
-for k in range(len(pca_lambda)):
-	sum_k += pca_lambda[k]
-	if(sum_k/total > 0.95):
-		dim = k+1
-		break
+	count_row = 0
 
-print dim'''
+	no_data=9635
+	no_samples=2225
+
+	train_data = np.zeros((no_samples, no_data))	# Sparse data matrix
+
+	for row in file_bbc_mtx:
+		if count_row > 1:
+			row  = row.split()
+			#print row
+			train_data[int(row[1])-1][int(row[0])-1] = float(row[2])
+
+		count_row += 1
+
+	new_train_data = create_normalized_data(no_data, no_samples, train_data)
+
+	return new_train_data, no_data, no_samples
+
+
+if __name__ == '__main__':
+
+
+	new_train_data, no_data, no_samples = create_vectors()
+
+	words = []	# List of words
+	words = create_word_list()
+
+	classes = []	# List having the classes for each of the document instance
+	classes = create_classes()
+
+	final_train_data, component_array = perform_pca(new_train_data, no_samples)
+
+	#print final_train_data.shape
+
+
